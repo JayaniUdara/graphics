89dacc2f9064b68ba6d4d28e668479d77d3a7255
Jayani Udara <107987496+JayaniUdara@users.noreply.github.com>
2023-02-17 08:42:47 +0530

    Add files via upload

---
+++ newone2.cpp
@@ -0,0 +1,301 @@
+#include <glut.h>
+#include <iostream>
+#include <cmath>
+#include <math.h>
+
+using namespace std;
+
+int ww = 600, wh = 600;  
+int first = 0;  
+int xi, yi, xf, yf;  
+
+const int INSIDE = 0; // 0000
+const int LEFT = 1;   // 0001
+const int RIGHT = 2;  // 0010
+const int BOTTOM = 4; // 0100
+const int TOP = 8;    // 1000
+
+const int x_max = 400;
+const int y_max = 400;
+const int x_min = 100;
+const int y_min = 100;
+
+int computeCode(int x, int y)
+{
+    int code = INSIDE;
+
+    if (x < x_min)      // to the left of rectangle
+        code |= LEFT;
+    else if (x > x_max) // to the right of rectangle
+        code |= RIGHT;
+    if (y < y_min)      // below the rectangle
+        code |= BOTTOM;
+    else if (y > y_max) // above the rectangle
+        code |= TOP;
+
+    return code;
+}
+
+
+
+void putPixel (int x, int y)  
+{  
+   glColor3f (0.3, 0.2, 0.0); // activate the pixel by setting the point color to white  
+   glBegin (GL_POINTS);  
+   glVertex2i (x, y); // set the point  
+   glEnd ();  
+   glFlush (); // process all openGL routines as quickly as possible  
+} 
+ 
+void display()  
+{  
+   glClearColor(0.4, 0.7, 0.5, 1.0);  
+   glColor3f(0.2, 0.3, 0.3); 
+   
+   glClear(GL_COLOR_BUFFER_BIT);
+   
+   glFlush();  
+}  
+void bresenhamAlg (int x0, int y0, int x1, int y1)  
+{  
+  int dx = abs (x1 - x0);  
+  int dy = abs (y1 - y0);  
+  int x, y;  
+  if (dx >= dy)  
+  {  
+   int d = 2*dy-dx;  
+   int ds = 2*dy;  
+   int dt = 2*(dy-dx);  
+       if (x0 < x1)   
+       {  
+            x = x0;  
+            y = y0;  
+        }  
+        else  
+        {   
+            x = x1;  
+            y = y1;  
+            x1 = x0;  
+            y1 = y0;  
+        }  
+  putPixel (x, y);  
+  while (x < x1)  
+  {  
+    if (d < 0)  
+        d += ds;  
+        else {  
+             if (y < y1) {  
+               y++;  
+               d += dt;  
+				}   
+             else {  
+				y--;  
+				d += dt;             
+			 }  
+		}  
+        x++;  
+		putPixel (x, y);  
+    }  
+}  
+       else {   
+           int d = 2*dx-dy;  
+             int ds = 2*dx;  
+             int dt = 2*(dx-dy);  
+             if (y0 < y1) 
+			 {  
+				 x = x0;  
+				 y = y0;  
+             }  
+             else 
+			 {   
+				 x = x1;  
+				 y = y1;  
+				 y1 = y0;  
+				x1 = x0;  
+             }  
+            putPixel (x, y);   
+        while (y < y1)  
+        {  
+              if (d < 0)  
+                 d += ds;  
+             else {  
+                   if (x < x1)
+				   {  
+						x++;  
+						d += dt;  
+					} else 
+					{  
+						x--;  
+						d += dt;  
+					}                
+				}  
+			y++;  
+             putPixel (x, y);
+			 
+       }        
+	}  
+}  
+
+void LiangBarskyClipping(float x0, float y0, float x1, float y1, float left, float right, float bottom, float top)
+{
+    float t0 = 0.0, t1 = 1.0;
+    float dx = x1 - x0, dy = y1 - y0;
+    float p, q, r;
+
+    for (int edge = 0; edge < 4; edge++)
+    {
+        if (edge == 0)
+        {
+            p = -dx;
+            q = x0 - left;
+        }
+        else if (edge == 1)
+        {
+            p = dx;
+            q = right - x0;
+        }
+        else if (edge == 2)
+        {
+            p = -dy;
+            q = y0 - bottom;
+        }
+        else
+        {
+            p = dy;
+            q = top - y0;
+        }
+
+        r = q / p;
+        if (p == 0 && q < 0)
+            return;
+
+        if (p < 0)
+        {
+            if (r > t1)
+                return;
+            else if (r > t0)
+                t0 = r;
+        }
+        else if (p > 0)
+        {
+            if (r < t0)
+                return;
+            else if (r < t1)
+                t1 = r;
+        }
+    }
+
+    float x_clipped = x0 + t0 * dx;
+    float y_clipped = y0 + t0 * dy;
+    float x_clipped_end = x0 + t1 * dx;
+    float y_clipped_end = y0 + t1 * dy;
+    // Draw clipped line using x_clipped, y_clipped and x_clipped_end, y_clipped_end
+	bresenhamAlg(x_clipped,y_clipped,x_clipped_end,y_clipped_end);
+}
+void cohenSutherlandClip(int x1, int y1, int x2, int y2)
+{
+    int code1 = computeCode(x1, y1);
+    int code2 = computeCode(x2, y2);
+
+    bool accept = false;
+
+    while (true) {
+        if ((code1 == 0) && (code2 == 0)) {
+            accept = true;
+            break;
+        }
+        else if (code1 & code2) {
+            break;
+        }
+        else {
+            int code_out;
+            int x, y;
+
+            if (code1 != 0)
+                code_out = code1;
+            else
+                code_out = code2;
+
+            if (code_out & TOP) {
+                x = x1 + (x2 - x1) * (y_max - y1) / (y2 - y1);
+                y = y_max;
+            }
+            else if (code_out & BOTTOM) {
+                x = x1 + (x2 - x1) * (y_min - y1) / (y2 - y1);
+                y = y_min;
+            }
+            else if (code_out & RIGHT) {
+                y = y1 + (y2 - y1) * (x_max - x1) / (x2 - x1);
+                x = x_max;
+            }
+            else if (code_out & LEFT) {
+                y = y1 + (y2 - y1) * (x_min - x1) / (x2 - x1);
+                x = x_min;
+            }
+
+            if (code_out == code1) {
+                x1 = x;
+                y1 = y;
+                code1 = computeCode(x1, y1);
+            }
+            else {
+                x2 = x;
+                y2 = y;
+                code2 = computeCode(x2, y2);
+            }
+			cout<<"After WIndow func "<<x1 <<" , "<< y1<<" , "<<x2<<" , "<<y2 <<endl;
+        }
+    }
+    if (accept) {
+        cout << "Line accepted from " << x1 << ", " << y1 << " to " << x2 << ", "<<y2<<endl;
+		bresenhamAlg ( x1, y1, x2, y2);
+	}
+}
+
+void mouse(int btn, int state, int x, int y)
+{
+	glBegin(GL_LINE_LOOP);
+		glVertex2i(x_min,y_max);
+		glVertex2i(x_max,y_max);
+		glVertex2i(x_max,y_min);
+		glVertex2i(x_min,y_min);
+	glEnd();
+	
+	if (btn == GLUT_LEFT_BUTTON)
+		if (state == GLUT_DOWN)
+		{
+			xi = x;
+			yi = (wh - y);
+		}
+		else if (state == GLUT_UP) {
+			xf = x;
+			yf = (wh - y);
+			glutPostRedisplay();
+			cout<<"Before WIndow func "<< xi <<" , "<< yi<<" , "<<xf<<" , "<<yf <<endl;
+			cohenSutherlandClip(xi, yi, xf, yf);
+		}
+	if (btn == GLUT_RIGHT_BUTTON) {
+		glClear(GL_COLOR_BUFFER_BIT);
+		glFlush();
+	}
+}
+void myinit()  
+{    
+     glViewport(0,0,ww,wh);  
+     glMatrixMode(GL_PROJECTION);  
+     glLoadIdentity();  
+     gluOrtho2D(0.0,(GLdouble)ww,0.0,(GLdouble)wh);  
+     glMatrixMode(GL_MODELVIEW);  
+}  
+int main(int argc, char** argv)  
+{  
+     glutInit(&argc,argv);  
+     glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);  
+     glutInitWindowSize(ww,wh);  
+     glutCreateWindow("Bresenham Line Algorithm");  
+     glutDisplayFunc(display);  
+     myinit();  
+     glutMouseFunc(mouse);  
+     glutMainLoop();  
+     return 0;  
+} 
